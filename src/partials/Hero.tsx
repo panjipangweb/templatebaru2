673bbbfff4e099566c1152511aa109a01b793cfc
Bran <111070465+panjipangweb@users.noreply.github.com>
2024-07-19 17:32:28 +0700

    Update Hero.tsx

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -10,21 +10,12 @@ const Hero = () => (
     <HeroAvatar
       title={
         <>
-          Hi there, I'm <GradientText>Ixartz</GradientText> 👋
+          Hi there, I'm <GradientText>Panji</GradientText> 👋
         </>
       }
       description={
         <>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            malesuada
-          </a>{' '}
-          nisi tellus, non imperdiet nisi tempor at. Lorem ipsum dolor sit amet,{' '}
-          <a className="text-cyan-400 hover:underline" href="/">
-            consectetur
-          </a>{' '}
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.
-        </>
+          An amateur wordsmith with a passion for science, documentary movies, tech, and languages.</>
       }
       avatar={
         <img
